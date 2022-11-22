a7897541e00952966c1b3cec20f2865d22fb4c91
Matías Pecchia <26371-matiaspecchia@users.noreply.gitlab.com>
2022-11-22 23:28:09 +0100

    test QemuOs introdiction for UpdateConfig

--- proxmox/config_qemu.go
+++ proxmox/config_qemu.go
@@ -304,6 +304,9 @@ func (config ConfigQemu) UpdateConfig(vmr *VmRef, client *Client) (err error) {
 
 	//Array to list deleted parameters
 	//deleteParams := []string{}
+	if config.QemuOs != "" {
+		configParams["ostype"] = config.QemuOs
+	}
 
 	if config.Agent != 0 {
 		configParams["agent"] = config.Agent
